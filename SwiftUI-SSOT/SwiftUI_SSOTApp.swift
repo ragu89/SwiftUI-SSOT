e5aa791d7a9b1f533b7979f1cff3fb6f01e2bad4
Guye Raphael, I233 <raphael.guye@post.ch>
2021-03-30 15:45:29 +0200

    Initial Commit

---
+++ SwiftUI-SSOT/SwiftUI_SSOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SSOTApp.swift
+//  SwiftUI-SSOT
+//
+//  Created by Guye Raphael, I233 on 30.03.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SSOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
